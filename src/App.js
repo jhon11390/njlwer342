1fba66e999a85efa21473419170042eb11f19b25
jhon11390 <meljhon2@gmail.com>
2020-04-03 21:41:46 -0500

    Se constructor y funciones para el formulario

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      tasks: [],
+      tasksinput: ''
+    }
+  }
+  
   render() {
     return (
       <div className="wrapper">
@@ -12,14 +21,34 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.tasks.map(task =>
+              <li>{task}</li>
+            )}
           </ul>
            <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.tasksinput} onChange={this.updateTask.bind(this)} onKeyPress={this.handlekeypress.bind(this)}/>
            </form>
         </div>
       </div>
     )
   }
+
+  updateTask(event) {
+    this.setState({
+      tasksinput: event.target.value
+    })
+  }
+  
+  handlekeypress(e) {
+    if(e.key === 'Enter') {
+      e.preventDefault();
+      this.setState({
+        tasks: this.state.tasks.concat(this.state.tasksinput),
+        tasksinput: ''
+      })
+    }
+  }
+
 }
 
 
